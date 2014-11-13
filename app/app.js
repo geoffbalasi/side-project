ef0dcc268b398adbe27c4e88c1c1bae131d63f51
Kevin Slattery <kevinrslattery@gmail.com>
2014-11-12 21:47:21 -0800

    Remove stylesheets from git and style clean up on app.js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
-'use strict';
+/*global angular, browser */
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.home',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
+(function () { 'use strict';
+
+    // Declare app level module which depends on views, and components
+    angular.module('myApp', [
+        'ngRoute',
+        'myApp.home',
+        'myApp.version'
+    ]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
+             }()
+);
