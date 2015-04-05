e7914abef274db47fa89ab43d2e7fbfccf59ae07
Kevin Slattery <kevinrslattery@gmail.com>
2015-04-05 16:40:41 -0700

    going to back pulling a local file since I am going to try to get this running on app engine.

--- app/js/Services/services.js
+++ app/js/Services/services.js
@@ -2,7 +2,7 @@ var servicesModule = angular.module('servicesModule', ['ngResource']);
 
 servicesModule.factory('CardService', ['$resource',
   function($resource){
-    return $resource('http://graceful-medley-89220.appspot.com/cards.json', {}, {
+    return $resource('cards.json', {}, {
       query: {method:'GET', isArray:true}
     });
   }]);
