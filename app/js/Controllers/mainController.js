3ef7dd9f41f85c8656e7790e1da52cb0c5848ec1
Kevin Slattery <kevinrslattery@gmail.com>
2015-07-12 00:23:33 -0700

    Load the API library and call one of the endpoints on our server from the sample project

--- app/js/Controllers/mainController.js
+++ app/js/Controllers/mainController.js
@@ -1,8 +1,25 @@
 'use strict';
+function init() {
+    window.init();
+}
 
 /* Card List Controllers */
 
-angular.module('controllersModule',[]).controller('mainController', ['$scope', 'CardService', '$window', function($scope, CardService, $window) {
+angular.module('controllersModule',[])
+  .controller('mainController', ['$scope', 'CardService', '$window', function($scope, CardService, $window) {
+    $window.init = function() {
+        console.log("got to window.init in mainController");
+        $scope.$apply($scope.initgapi);
+    };
+    $scope.initgapi = function() {
+        console.log("in initgapi");
+        var ROOT = '//' + window.location.host + '/_ah/api';
+        gapi.client.load('alwaysnext', 'v1', function() {
+                 gapi.client.alwaysnext.greetings.listGreeting().execute(function(resp){
+                     alert(resp);
+                 });
+        }, ROOT);
+    };
     $scope.cards = CardService.query();
     $scope.name = 'AlwaysNext';
 
